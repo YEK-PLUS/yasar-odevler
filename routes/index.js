334569b1fbeb7077c26df6d99c66cb62cb40bda3
YEK-PLUS <yunusemre@sariilanlar.com>
2020-12-12 16:24:18 +0300

    express server updated

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get("/", function (req, res, next) {
+  res.render("index");
 });
-router.get('/MBBP1115-2021G-1', function(req, res, next) {
-  res.render('MBBP1115-2021G-1');
+router.get("/MBBP1115-2021G-1", function (req, res, next) {
+  res.render("MBBP1115-2021G-1");
 });
-router.get('/MBBP1115-2021G-1/odev-1', function(req, res, next) {
-  res.render('MBBP1115-2021G-1/odev-1');
+router.get("/MBBP1115-2021G-1/:odev", function (
+  { params: { odev } },
+  res,
+  next
+) {
+  res.render(`MBBP1115-2021G-1/${odev}`);
 });
 
 module.exports = router;
